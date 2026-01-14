a3799e33b7430f9bed678909eadc373cc6a7752e
Raj6612 <rajy21285@gmail.com>
2026-01-14 18:16:11 +0530

    initial commit

---
+++ routes/foodRoute.js
@@ -0,0 +1,20 @@
+import express from 'express';
+const router = express.Router();
+import { addFood, listFood, removeFood } from '../controllers/foodController.js';
+import multer from 'multer';
+
+// image storage
+const storage = multer.diskStorage({
+    destination: "uploads",
+    filename:(req, file, cb) => {
+        return cb(null, `${Date.now()}${file.originalname}`);
+    }
+});
+
+const upload = multer({storage:storage});
+
+router.post("/add", upload.single("image"), addFood);
+router.get("/list", listFood);
+router.post("/remove", removeFood)
+
+export default router;
---
+++ server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import { connetcDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+
+// app config
+const app = express();
+const port = 4000;
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// db connection
+connetcDB();
+
+// rest api's endpoints
+app.use("/food", foodRouter);
+app.use("/images", express.static("uploads"));
+
+app.get("/", (req, res) => {
+  res.send("App is running");
+});
+
+app.listen(port, () => {
+  console.log(`Running on Link: ${`http://localhost:4000`}`);
+});
